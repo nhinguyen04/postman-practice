47ef091f8250ef879070152d4fe12a05bb77dd92
nhinguyen04 <valentinetwin94@gmail.com>
2023-04-28 16:56:40 -0700

    completed 08-fetch-long-practice

---
+++ practice-for-week-08-fetch-long-practice/fetch-requests.js
@@ -0,0 +1,51 @@
+/*
+Make fetch requests in the browser for each of the following phases.
+Paste your code for fetch requests here once you finish each phase.
+*/
+
+/* ============================== Phase 1 ============================== */
+
+// Your code here
+fetch('/products', {
+    method: 'POST',
+    body: 'name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+});
+
+
+
+/* ============================== Phase 2 ============================== */
+
+// Your code here
+fetch('/products', {
+    method: 'POST',
+    body: 'name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+})
+    .then(res => {
+        console.log(res.status);
+        console.log(res.headers.get('Content-Type'));
+        console.log(res.url);
+    });
+
+
+
+/* ============================== Phase 3 ============================== */
+
+// Your code here
+fetch('/products', {
+    method: 'POST',
+    body: new URLSearchParams({
+        name: 'Caibbean Delight Coffee',
+        description: 'Made by Manatee Coffee',
+        price: 11.99,
+        categories: 'grocery'
+    }),
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+});
